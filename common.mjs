4b01f540d35985f3f48fc599826f03e1d25f3bc1
Seddiq Azam <sedazam@gmail.com>
2026-02-24 14:54:59 +0000

    Fix parameter names in addDays function for clarity

--- common.mjs
+++ common.mjs
@@ -5,7 +5,7 @@ export function getUserIds() {
 export function getRevisionDates(dateString) {
   const [year, month, day] = dateString.split("-").map(Number);
 
-  function addDays(date, days) {
+  function addDays(y, m, d, days) {
     const date = new Date(year, month - 1, days);
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
   }
